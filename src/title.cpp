86cbe7bb8bcf7b1440430d4787ca4d2c9b023d22
Connor Clark <cjamcl@gmail.com>
2022-03-27 18:44:37 -0700

    emscripten persist save file

--- src/title.cpp
+++ src/title.cpp
@@ -34,6 +34,10 @@
 #include "mem_debug.h"
 #include "ffscript.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
+
 #ifdef _MSC_VER
 #define strupr _strupr
 #define stricmp _stricmp
@@ -2740,6 +2744,13 @@ int32_t save_savedgames()
 		
 	fclose(f2);
 	zc_free(iname);
+
+#ifdef __EMSCRIPTEN__
+	EM_ASM({
+		FS.syncfs(false, () => {});
+	});
+#endif
+
 	return ret;
 }
 
