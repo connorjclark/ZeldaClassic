bc6d921c4f92a54c17ec16622e613673a0690c70
Connor Clark <cjamcl@gmail.com>
2022-03-30 18:11:36 -0700

    emscripten: load a quest from url, creating a save file if needed

--- src/emscripten_utils.h
+++ src/emscripten_utils.h
@@ -1,8 +1,16 @@
 #ifndef __emscripten_utils_h_
 #define __emscripten_utils_h_
 
+#include <string>
+
+struct QueryParams
+{
+  std::string quest;
+};
+
 void init_fs_em();
 void sync_fs_em();
 void fetch_quest_em(const char *path);
+QueryParams get_query_params();
 
 #endif
