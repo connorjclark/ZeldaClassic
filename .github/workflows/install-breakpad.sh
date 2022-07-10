0ff50ed4258d4c07cdf20e37fbe69a12ceff0739
Connor Clark <cjamcl@gmail.com>
2022-07-10 02:43:44 -0700

    update

--- .github/workflows/install-breakpad.sh
+++ .github/workflows/install-breakpad.sh
@@ -4,14 +4,13 @@ set -euxo pipefail
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
 
-mkdir breakpad && cd breakpad
+mkdir breakpad2 && cd breakpad2
 cmd.exe /c '..\depot_tools\fetch breakpad'
 echo "0---"
 ls
 echo "1---"
 cd src
-# I think it returns exit code 1 because the .gyp has cycles?
-cmd.exe /c '..\..\depot_tools\python2-bin\python2 .\src\tools\gyp\gyp_main.py src\client\windows\breakpad_client.gyp' || true
+cmd.exe /c '..\..\depot_tools\python2-bin\python2 .\src\tools\gyp\gyp_main.py --no-circular-check src\client\windows\breakpad_client.gyp'
 echo "2---"
 ls
 echo "3---"
@@ -19,7 +18,7 @@ ls src/client/windows/
 
 # echo $(which msbuild)
 pwd
-cmd.exe /c 'msbuild src\client\windows\breakpad_client.sln -t:rebuild -verbosity:diag -property:Configuration=Release'
+cmd.exe /c 'msbuild src\client\windows\breakpad_client.sln /p:PlatformToolset=v143 -t:rebuild -verbosity:diag -property:Configuration=Release'
 ls
 
 # ./configure && make
