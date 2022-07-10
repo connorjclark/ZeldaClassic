6355de48148d8652880ac13e1fe07ed8967d23ff
Connor Clark <cjamcl@gmail.com>
2022-07-10 01:05:44 -0700

    update

--- .github/workflows/install-breakpad.sh
+++ .github/workflows/install-breakpad.sh
@@ -6,11 +6,18 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot
 
 mkdir breakpad && cd breakpad
 cmd.exe /c '..\depot_tools\fetch breakpad'
+echo "0---"
 ls
+echo "1---"
 cd src
-ls
 cmd.exe /c '..\..\depot_tools\python2-bin\python2 .\src\tools\gyp\gyp_main.py src\client\windows\breakpad_client.gyp'
+echo "2---"
+ls
+echo "3---"
+ls src/client/windows/
 
+echo $(which msbuild)
+pwd
 msbuild src/client/windows/build_all.vcproj -t:rebuild -verbosity:diag -property:Configuration=Release
 ls
 
