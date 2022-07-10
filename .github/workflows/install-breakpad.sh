0035906523ecafcbf69e146a586764303865fbff
Connor Clark <cjamcl@gmail.com>
2022-07-09 21:10:41 -0700

    update

--- .github/workflows/install-breakpad.sh
+++ .github/workflows/install-breakpad.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
 export PATH=$PATH:$PWD/depot_tools
+set DEPOT_TOOLS_UPDATE=1
 
 mkdir breakpad && cd breakpad
 fetch breakpad
