be0b35b0212723dd6ea8e6ed7401398d85e76b17
Connor Clark <cjamcl@gmail.com>
2022-07-09 21:33:56 -0700

    update

--- .github/workflows/install-breakpad.sh
+++ .github/workflows/install-breakpad.sh
@@ -3,10 +3,10 @@
 set -euxo pipefail
 
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
-export PATH=$PATH:$PWD/depot_tools
+export PATH=$PWD/depot_tools:$PATH
 
 # Why is depot_tool's python locater so busted?
-cp $(which python3) depot_tools/python3.exe
+# cp $(which python3) depot_tools/python3.exe
 
 mkdir breakpad && cd breakpad
 fetch breakpad
