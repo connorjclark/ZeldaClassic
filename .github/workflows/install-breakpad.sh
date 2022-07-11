6967efe0a70ef3a059d8b860b6fef6cac002c984
Connor Clark <cjamcl@gmail.com>
2022-07-10 18:11:48 -0700

    ugh

--- .github/workflows/install-breakpad.sh
+++ .github/workflows/install-breakpad.sh
@@ -2,24 +2,33 @@
 
 set -euxo pipefail
 
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
+# git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot_tools
+setx path "%path%;$PWD\depot_tools"
 
-mkdir breakpad && cd breakpad
-cmd.exe /c '..\depot_tools\fetch breakpad'
-echo "0---"
-ls
-echo "1---"
-cd src
-cmd.exe /c '..\..\depot_tools\python2-bin\python2 .\src\tools\gyp\gyp_main.py --no-circular-check src\client\windows\breakpad_client.gyp'
-echo "2---"
-ls
-echo "3---"
-ls src/client/windows/
+mkdir crashpad && cd crashpad
+fetch crashpad
+cd crashpad
+gn gen out/Default
+ninja -C out/Default
 
-# echo $(which msbuild)
-pwd
-cmd.exe /c 'msbuild src\client\windows\breakpad_client.sln -tv:16.0 -t:rebuild -verbosity:diag -property:Configuration=Release'
-ls
+# https://stackoverflow.com/questions/26191004/how-do-i-integrate-google-crashpad-with-my-application
+
+# mkdir breakpad && cd breakpad
+# cmd.exe /c '..\depot_tools\fetch breakpad'
+# echo "0---"
+# ls
+# echo "1---"
+# cd src
+# cmd.exe /c '..\..\depot_tools\python2-bin\python2 .\src\tools\gyp\gyp_main.py --no-circular-check src\client\windows\breakpad_client.gyp'
+# echo "2---"
+# ls
+# echo "3---"
+# ls src/client/windows/
+
+# # echo $(which msbuild)
+# pwd
+# cmd.exe /c 'msbuild src\client\windows\breakpad_client.sln -tv:16.0 -t:rebuild -verbosity:diag -property:Configuration=Release'
+# ls
 
 # ./configure && make
 # make install
--- src/zapp.cpp
+++ src/zapp.cpp
@@ -5,31 +5,113 @@
 #include <unistd.h>
 #endif
 
-#ifdef HAS_BREAKPAD
-#include "client/windows/sender/crash_report_sender.h"
-#include "client/windows/handler/exception_handler.h"
+#ifdef HAS_CRASHPAD
 
-bool minidump_callback(const wchar_t* dump_path, const wchar_t* minidump_id, void* context, EXCEPTION_POINTERS* exinfo, MDRawAssertionInfo* assertion, bool succeeded)
-{
-  google_breakpad::CrashReportSender sender(L"crash.checkpoint");
+#include <client/crash_report_database.h>
+#include <client/settings.h>
+#include <client/crashpad_client.h>
 
-  std::wstring filename = L"_dump_path_";
-  filename += L"\\";
-  filename += minidump_id;
-  filename += L".dmp";
+typedef std::wstring StringType;
 
-  std::map<std::wstring, std::wstring> files;
-  files.insert(std::make_pair(filename, filename));
+using namespace base;
+using namespace crashpad;
+using namespace std;
 
-  // At this point you may include custom data to be part of the crash report.
-  std::map<std::wstring, std::wstring> userCustomData;
-  userCustomData.insert(std::make_pair(L"desc", L"Hello World"));
 
-  sender.SendCrashReport(L"https://api.raygun.com/entries/breakpad?apikey=o841YPlfAEh7AuZyVLw", userCustomData, files, 0);
+StringType getExecutableDir() {
+  HMODULE hModule = GetModuleHandleW(NULL);
+  WCHAR path[MAX_PATH];
+  DWORD retVal = GetModuleFileNameW(hModule, path, MAX_PATH);
+  if (retVal == 0) return NULL;
 
-  return true;
+  wchar_t *lastBackslash = wcsrchr(path, '\\');
+  if (lastBackslash == NULL) return NULL;
+  *lastBackslash = 0;
+
+  return path;
+//   return std::filesystem::path(path).string();
 }
+
+bool initializeCrashpad() {
+  // Get directory where the exe lives so we can pass a full path to handler, reportsDir, metricsDir and attachments
+  StringType exeDir = getExecutableDir();
+
+  // Ensure that handler is shipped with your application
+  base::FilePath handler(exeDir + L"/path/to/crashpad_handler");
+
+  // Directory where reports will be saved. Important! Must be writable or crashpad_handler will crash.
+  base::FilePath reportsDir(exeDir + L"/path/to/crashpad");
+
+  // Directory where metrics will be saved. Important! Must be writable or crashpad_handler will crash.
+  base::FilePath metricsDir(exeDir + L"/path/to/crashpad");
+
+  // Configure url with BugSplat’s public fred database. Replace 'fred' with the name of your BugSplat database.
+  std::string url = "https://fred.bugsplat.com/post/bp/crash/crashpad.php";
+
+  // Metadata that will be posted to the server with the crash report map
+  map<std::string, std::string> annotations;
+  annotations["format"] = "minidump";           // Required: Crashpad setting to save crash as a minidump
+  annotations["database"] = "fred";             // Required: BugSplat appName
+  annotations["product"] = "myCrashpadCrasher"; // Required: BugSplat appName
+  annotations["version"] = "1.0.0";             // Required: BugSplat appVersion
+  annotations["key"] = "Sample key";            // Optional: BugSplat key field
+  annotations["user"] = "fred@bugsplat.com";    // Optional: BugSplat user email
+  annotations["list_annotations"] = "Sample comment"; // Optional: BugSplat crash description
+
+  // Disable crashpad rate limiting so that all crashes have dmp files
+  vector<std::string> arguments; 
+  arguments.push_back("--no-rate-limit");
+
+  // Initialize Crashpad database
+  unique_ptr<CrashReportDatabase> database = CrashReportDatabase::Initialize(reportsDir);
+  if (database == NULL) return false;
+
+  // File paths of attachments to uploaded with minidump file at crash time - default upload limit is 2MB
+  vector<FilePath> attachments;
+  FilePath attachment(exeDir + L"/path/to/attachment.txt");
+  attachments.push_back(attachment);
+
+  // Enable automated crash uploads
+  Settings *settings = database->GetSettings();
+  if (settings == NULL) return false;
+  settings->SetUploadsEnabled(true);
+
+  // Start crash handler
+//   CrashpadClient *client = new CrashpadClient();
+//   bool status = client->StartHandler(handler, reportsDir, metricsDir, url, annotations, arguments, true, true, attachments);
+//   return status;
+    return 0;
+}
+
 #endif
+
+
+
+// #ifdef HAS_BREAKPAD
+// #include "client/windows/sender/crash_report_sender.h"
+// #include "client/windows/handler/exception_handler.h"
+
+// bool minidump_callback(const wchar_t* dump_path, const wchar_t* minidump_id, void* context, EXCEPTION_POINTERS* exinfo, MDRawAssertionInfo* assertion, bool succeeded)
+// {
+//   google_breakpad::CrashReportSender sender(L"crash.checkpoint");
+
+//   std::string filename = L"_dump_path_";
+//   filename += L"\\";
+//   filename += minidump_id;
+//   filename += L".dmp";
+
+//   std::map<std::string, std::string> files;
+//   files.insert(std::make_pair(filename, filename));
+
+//   // At this point you may include custom data to be part of the crash report.
+//   std::map<std::string, std::string> userCustomData;
+//   userCustomData.insert(std::make_pair(L"desc", L"Hello World"));
+
+//   sender.SendCrashReport(L"https://api.raygun.com/entries/breakpad?apikey=o841YPlfAEh7AuZyVLw", userCustomData, files, 0);
+
+//   return true;
+// }
+// #endif
 
 bool is_in_osx_application_bundle()
 {
@@ -49,6 +131,10 @@ void crash(int b)
 
 void common_main_setup(int argc, char **argv)
 {
+#ifdef HAS_CRASHPAD
+    initializeCrashpad();
+#endif
+
     // This allows for opening a binary from Finder and having ZC be in its expected
     // working directory (the same as the binary). Only used when not inside an application bundle,
     // and only for testing purposes really. See comment about `SKIP_APP_BUNDLE` in buildpack_osx.sh
@@ -58,11 +144,6 @@ void common_main_setup(int argc, char **argv)
     }
 #endif
 
-#ifdef HAS_BREAKPAD
-    google_breakpad::ExceptionHandler *pHandler = new google_breakpad::ExceptionHandler(
-        L"_dump_path_", 0, minidump_callback, 0,
-        google_breakpad::ExceptionHandler::HANDLER_ALL, MiniDumpNormal, L"", 0);
-#endif
     // https://stackoverflow.com/questions/45733174/breakpad-exception-handler-not-used-in-a-dll-on-windows
     // __try 
     // {
@@ -73,14 +154,15 @@ void common_main_setup(int argc, char **argv)
     //     delete pHandler;
     // }
 
-    try 
-    {
-        crash(0);
-    }
-    catch(std::exception &ex)
-    { 
-        delete pHandler;
-    }
+    crash(0);
+    // try 
+    // {
+    //     crash(0);
+    // }
+    // catch(std::exception &ex)
+    // { 
+    //     delete pHandler;
+    // }
 
 
 }
