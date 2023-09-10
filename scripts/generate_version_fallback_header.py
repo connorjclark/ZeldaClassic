c8fea42aa323f940a2786fdf7de39cec83ce65aa
Connor Clark <cjamcl@gmail.com>
2023-09-09 21:52:27 -0700

    wip: version refactor

--- scripts/generate_version_fallback_header.py
+++ scripts/generate_version_fallback_header.py
@@ -7,5 +7,9 @@ root_dir = script_dir.parent
 header_path = root_dir / 'src/base/version_fallback.h'
 
 command = 'git describe --tags --abbrev=0 --match *.*.*'
-version = subprocess.check_output(command, shell=True, encoding='utf-8').strip()
+try:
+    version = subprocess.check_output(command, shell=True, encoding='utf-8').strip()
+except:
+    # TODO: remove once first version is posted.
+    version = '3.0.0'
 header_path.write_text(f'#define ZC_VERSION_FALLBACK "{version}"\n')
