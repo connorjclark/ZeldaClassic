6b3987a5f9035faa653b67e4886bea2a0becf2c9
Connor Clark <cjamcl@gmail.com>
2024-07-04 18:24:38 -0700

    refactor!: make include header guards standards compliant, normalize a bit

--- src/zc/zelda.cpp
+++ src/zc/zelda.cpp
@@ -4294,8 +4294,12 @@ int main(int argc, char **argv)
 
 	if (used_switch(argc, argv, "-upload-replays"))
 	{
+#ifdef HAS_CURL
 		replay_upload();
 		return 0;
+#else
+		return 1;
+#endif
 	}
 
 	int only_arg = used_switch(argc, argv, "-only");
