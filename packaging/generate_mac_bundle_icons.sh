b2299267b6c9e015e69bd4d84374be9b60c7c058
Connor Clark <cjamcl@gmail.com>
2025-11-21 12:26:48 -0800

    wip

---
+++ packaging/generate_mac_bundle_icons.sh
@@ -0,0 +1,24 @@
+# Only run to re-generate icons.icns if the icon to use changes.
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT=$( dirname "$DIR" )
+
+# Generate icon.
+ICONDIR="icons.iconset"
+ICON=resources/assets/zc/ZC_Icon_Medium_Launcher.png
+
+mkdir "$ICONDIR"
+
+# Normal screen icons
+for size in 16 32 64 128 256 512; do
+sips -z $size $size $ICON --out "$ICONDIR/icon_${size}x${size}.png" ;
+done
+
+# Retina display icons
+for size in 32 64 256 512; do
+sips -z $size $size $ICON --out "$ICONDIR"/icon_$(expr $size / 2)x$(expr $size / 2)x2.png ;
+done
+
+# Make a multi-resolution Icon
+iconutil -c icns -o "$ROOT/packaging/icons.icns" "$ICONDIR"
+rm -rf "$ICONDIR"
