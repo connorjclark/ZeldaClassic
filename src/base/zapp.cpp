b7056f7b8c4b0b8363409fd807f5a70d395a3f6e
Connor Clark <cjamcl@gmail.com>
2025-11-30 00:20:20 -0800

    wip

--- src/base/zapp.cpp
+++ src/base/zapp.cpp
@@ -131,9 +131,11 @@ void common_main_setup(App id, int argc_, char **argv_)
 
     // This allows for opening a binary from Finder and having ZC be in its expected
     // working directory (the same as the binary). Only used when not inside an application bundle,
-    // and only for testing purposes really. See comment about `SKIP_APP_BUNDLE` in package_mac.sh
+    // and only for testing purposes really.
+    // TODO ! rm? test zscript vscode
 #ifdef __APPLE__
     if (std::getenv("ZC_DISABLE_OSX_CHDIR") == nullptr && !is_in_osx_application_bundle() && argc > 0) {
+        printf("changing cwd to Resources folder\n");
         chdir(std::filesystem::path(argv[0]).parent_path().c_str());
     }
 #endif
