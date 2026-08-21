ZQuestClassic/ZQuestClassic

commit 21773b636c7fb76ed9d1ecd0e032d6d143ee692a

misc!: fix archive download

scripts/archives.py
@@ -60,7 +60,7 @@ class Revision:
                 print(e)
                 return None
         else:
-            dir = _download(self, release_platform)
+            dir = download(self.tag, release_platform)
 
         return create_binary_paths(dir, release_platform)
 

scripts/build_historical.py
@@ -320,7 +320,7 @@ def build_locally(revision: Revision, release_platform: str):
             most_recent_rev = get_most_recent_rev(revision, release_platform)
             if not most_recent_rev:
                 raise Exception('could not find recent build to steal files from')
-            rls_dir = archives.download(most_recent_rev, release_platform)
+            rls_dir = archives.download(most_recent_rev.tag, release_platform)
             shutil.copyfile(rls_dir / need, package_dir / need)
 
         # Modules require tons of handholding to get right. Oh, and at some point the files needed to run the program were no longer
@@ -445,7 +445,7 @@ def build_locally(revision: Revision, release_platform: str):
         ):
             most_recent_rev = get_most_recent_rev(revision, release_platform)
             if most_recent_rev:
-                rls_dir = archives.download(most_recent_rev, release_platform)
+                rls_dir = archives.download(most_recent_rev.tag, release_platform)
                 for path in (rls_dir / 'modules/classic').glob('*.*'):
                     shutil.copyfile(
                         path, package_dir / path.name.replace('classic_', '')

connorjclark/ZeldaClassic

commit 8ae436142d4b33b24afe72fc955474a848338e1d

misc!: download to the revision's own directory in Revision.binaries
end changelog

Revision.binaries went through download(), which re-derives the
revision type from the tag's length instead of using self.type. For a
tag-named test build from the bucket (ex the old 2.55-alpha-16 entry),
files were downloaded to .tmp/archives/release/<tag> while
Revision.dir() - used everywhere else - points at
.tmp/archives/test/<tag>. Call download_revision directly with self.

Regressed in 21773b636c.

scripts/archives.py
@@ -60,7 +60,7 @@ class Revision:
                 print(e)
                 return None
         else:
-            dir = download(self.tag, release_platform)
+            dir = download_revision(self, release_platform)
 
         return create_binary_paths(dir, release_platform)
 